0030759a822b0212494add20bfce62e417cd0784
Piotr Rak <piotr.rak@gmail.com>
2020-09-23 18:54:17 +0200

    Deal with ff_const59 using trait
    Intruduce avcpp_const59<T> which deals with ustream changes.
    It detects if we should using const types or not. It bases
    detection on the return type of one of offending fuctions.

--- src/format.h
+++ src/format.h
@@ -5,19 +5,20 @@
 
 #include "ffmpeg.h"
 
-// ff_const59 is not part of public API
-#if AVCPP_API_AVIOFORMAT
-#define avcpp_format_const
-#else
-#define avcpp_format_const const
-#endif
-
 namespace av {
 
 using FmtCodec = class Codec;
 namespace internal {
+template <typename T>
+inline static constexpr bool is_const_pointer_v = std::is_const_v<std::remove_pointer_t<T>>;
+
 bool codec_supported(const AVCodecTag *const *codecTag, const FmtCodec &codec);
 } // ::internal
+
+// ff_const59 is not part of public API
+template <typename T>
+using avcpp_const59 = std::conditional_t<
+    internal::is_const_pointer_v<decltype(av_find_input_format(nullptr))>, const T, T>;
 
 template<typename T>
 struct Format : public FFWrapperPtr<T>
@@ -43,7 +44,7 @@ struct Format : public FFWrapperPtr<T>
         return false;
     }
 
-    void setFormat(avcpp_format_const T* format) noexcept {
+    void setFormat(T *format) noexcept {
         FFWrapperPtr<T>::reset(format);
     }
 
@@ -58,11 +59,11 @@ protected:
     using FFWrapperPtr<T>::m_raw;
 };
 
-class InputFormat : public Format<avcpp_format_const AVInputFormat>
+class InputFormat : public Format<avcpp_const59<AVInputFormat>>
 {
 public:
-    using Format<avcpp_format_const AVInputFormat>::Format;
-    using Format<avcpp_format_const AVInputFormat>::setFormat;
+    using Format<avcpp_const59<AVInputFormat>>::Format;
+    using Format<avcpp_const59<AVInputFormat>>::setFormat;
 
     InputFormat() = default;
 
@@ -73,11 +74,11 @@ public:
 };
 
 
-class OutputFormat : public Format<avcpp_format_const AVOutputFormat>
+class OutputFormat : public Format<avcpp_const59<AVOutputFormat>>
 {
 public:
-    using Format<avcpp_format_const AVOutputFormat>::Format;
-    using Format<avcpp_format_const AVOutputFormat>::setFormat;
+    using Format<avcpp_const59<AVOutputFormat>>::Format;
+    using Format<avcpp_const59<AVOutputFormat>>::setFormat;
 
     OutputFormat() = default;
 
